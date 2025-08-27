163976ba49ab34e73e930e7aaf25fb7b39aaee61
Oleg A. Khlybov <fougas@mail.ru>
2025-08-27 16:11:46 +0500

    Command line processing

--- app-cli/src/main/java/app/cli/Main.java
+++ app-cli/src/main/java/app/cli/Main.java
@@ -1,10 +1,20 @@
 package app.cli;
 
 import app.*;
+import picocli.*;
+import picocli.CommandLine.*;    
 
-public class Main {
+// https://picocli.info/quick-guide.html#_what_is_picocli 
+
+@Command(name = "app", version = "0.0.1", mixinStandardHelpOptions = true)
+public class Main implements Runnable {
 
   public static void main(String[] args) {
+    new CommandLine(new Main()).execute(args);
+  }
+  
+  @Override
+  public void run() {
     System.out.println(new Runner());
   }
   
--- app-cli/src/main/java/module-info.java
+++ app-cli/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module app.cli {
   requires app;
   exports app.cli;
+  requires info.picocli;
+  opens app.cli to info.picocli;
 }
